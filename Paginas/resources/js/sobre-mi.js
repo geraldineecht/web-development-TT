f32ece6fc4d0f46fce67ea7fa266ad80696b4510
Cristina Hernandez <A00830775@tec.mx>
2022-05-04 16:29:35 -0500

    Update sobre-mi.js
    - No se guardo la funcion lol

--- Paginas/resources/js/sobre-mi.js
+++ Paginas/resources/js/sobre-mi.js
@@ -1,9 +1,3 @@
-
-ta.addEventListener("input", function (e) {
-    count.innerHTML = this.value.length;
-});
-
-
 function countChar() {
     var count = document.getElementById("about-input").value.length;
     document.getElementById("current").innerHTML = count;
