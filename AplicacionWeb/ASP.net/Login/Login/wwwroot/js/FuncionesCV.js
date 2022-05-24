4ba3b1679225c661a2beecebba23ddc1099ec136
alexdennis2001 <alejandrodennis2001@outlook.com>
2022-05-24 12:49:49 -0600

    Funcionalidad habilidades

--- AplicacionWeb/ASP.net/Login/Login/wwwroot/js/FuncionesCV.js
+++ AplicacionWeb/ASP.net/Login/Login/wwwroot/js/FuncionesCV.js
@@ -15,3 +15,34 @@ function checkBox() {
         document.getElementById('SalidaMes').disabled = false;
     }
 }
+
+function addAbility() {
+    var elem = document.getElementById('remover');
+    elem.parentNode.removeChild(elem);
+
+    var x = document.getElementById("addAbility");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    }
+
+    var y = document.getElementById("last-button");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    }
+
+}
+
+function addAbility2() {
+    var elem = document.getElementById('last-button');
+    elem.parentNode.removeChild(elem);
+
+    var x = document.getElementById("addAbility2");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    }
+
+}
+
+
+
+
