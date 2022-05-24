1f98a2f3e6787f24e53a9de14f208c3069b6ef3a
alexdennis2001 <alejandrodennis2001@outlook.com>
2022-05-23 19:24:49 -0600

    Funcionalidad casi terminada

---
+++ AplicacionWeb/ASP.net/Login/Login/wwwroot/js/FuncionesCV.js
@@ -0,0 +1,17 @@
+﻿function countChar() {
+    var count = document.getElementById("about-input").value.length;
+    document.getElementById("current").innerHTML = count;
+}
+
+function checkBox() {
+    var checkBox = document.getElementById("check");
+
+    if (checkBox.checked) {
+        document.getElementById('SalidaAnio').disabled = true;
+        document.getElementById('SalidaMes').disabled = true;
+    }
+    else {
+        document.getElementById('SalidaAnio').disabled = false;
+        document.getElementById('SalidaMes').disabled = false;
+    }
+}
--- AplicacionWeb/ASP.net/Login/Login/wwwroot/js/Sobre_mi.js
+++
@@ -1,4 +0,0 @@
-﻿function countChar() {
-    var count = document.getElementById("about-input").value.length;
-    document.getElementById("current").innerHTML = count;
-}
