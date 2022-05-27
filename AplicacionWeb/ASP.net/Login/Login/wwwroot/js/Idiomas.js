d452e4039530390cd52bff9d9e8cf3e192b416c6
alexdennis2001 <alejandrodennis2001@outlook.com>
2022-05-26 23:09:56 -0600

    Funcionalidad terminada

--- AplicacionWeb/ASP.net/Login/Login/wwwroot/js/Idiomas.js
+++ AplicacionWeb/ASP.net/Login/Login/wwwroot/js/Idiomas.js
@@ -4,6 +4,45 @@ function addIdioma3() {
     elem.parentNode.removeChild(elem);
 
     var x = document.getElementById("addLanguage3");
+    var y = document.getElementById("button2");
+    if (x.style.display === "none") {
+        x.style.display = "";
+        y.style.display = "";
+    }
+
+}
+
+function addIdioma4() {
+    var elem = document.getElementById('button2');
+    elem.parentNode.removeChild(elem);
+
+    var x = document.getElementById("addLanguage4");
+    var y = document.getElementById("button3");
+    if (x.style.display === "none") {
+        x.style.display = "";
+        y.style.display = "";
+    }
+
+}
+
+function addIdioma5() {
+    var elem = document.getElementById('button3');
+    elem.parentNode.removeChild(elem);
+
+    var x = document.getElementById("addLanguage5");
+    var y = document.getElementById("button4");
+    if (x.style.display === "none") {
+        x.style.display = "";
+        y.style.display = "";
+    }
+
+}
+
+function addIdioma6() {
+    var elem = document.getElementById('button4');
+    elem.parentNode.removeChild(elem);
+
+    var x = document.getElementById("addLanguage6");
     if (x.style.display === "none") {
         x.style.display = "";
     }
