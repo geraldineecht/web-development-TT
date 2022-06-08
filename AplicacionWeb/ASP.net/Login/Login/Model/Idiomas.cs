8566a94386bab69fc3fe5493e20439a283710cf1
alexdennis2001 <alejandrodennis2001@outlook.com>
2022-06-07 22:17:50 -0500

    CV Backend Terminado

---
+++ AplicacionWeb/ASP.net/Login/Login/Model/Idiomas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atos.Model
+{
+    public class Idiomas
+    {
+        public string Idioma { get; set; }
+        public string Nivel { get; set; }
+    }
+}
--- AplicacionWeb/ASP.net/Login/Login/Pages/CV.cshtml.cs
+++ AplicacionWeb/ASP.net/Login/Login/Pages/CV.cshtml.cs
@@ -8,12 +8,13 @@ using Microsoft.AspNetCore.Http;
 using MySql.Data.MySqlClient;
 using Microsoft.Extensions.Configuration;
 using Login.Model;
+using Atos.Model;
 
 namespace Atos.Pages
 {
     public class CVModel : PageModel
     {
-        int idAplicante = 3;
+        int idAplicante = 7;
 
         private readonly IConfiguration _configuration;
         public CVModel(IConfiguration configuration)
@@ -67,6 +68,61 @@ namespace Atos.Pages
         public string Habilidad4 { get; set; }
         [BindProperty]
         public string Habilidad5 { get; set; }
+        [BindProperty]
+        public int PorcHabilidad1 { get; set; }
+        [BindProperty]
+        public int PorcHabilidad2 { get; set; }
+        [BindProperty]
+        public int PorcHabilidad3 { get; set; }
+        [BindProperty]
+        public int PorcHabilidad4 { get; set; }
+        [BindProperty]
+        public int PorcHabilidad5 { get; set; }
+        [BindProperty]
+        public string PorcHabilidad1_ { get; set; }
+        [BindProperty]
+        public string PorcHabilidad2_ { get; set; }
+        [BindProperty]
+        public string PorcHabilidad3_ { get; set; }
+        [BindProperty]
+        public string PorcHabilidad4_ { get; set; }
+        [BindProperty]
+        public string PorcHabilidad5_ { get; set; }
+        [BindProperty]
+        public string PuestoExperiencia { get; set; }
+        [BindProperty]
+        public string EmpresaExperiencia { get; set; }
+        [BindProperty]
+        public string AnioExperienciaInicio { get; set; }
+        [BindProperty]
+        public string AnioExperienciaFin { get; set; }
+        [BindProperty]
+        public string Actividades { get; set; }
+        [BindProperty]
+        public string MesExperienciaInicio { get; set; }
+        [BindProperty]
+        public string MesExperienciaFin { get; set; }
+        [BindProperty]
+        public string Institucion { get; set; }
+        [BindProperty]
+        public string Carrera { get; set; }
+        [BindProperty]
+        public string PaisFormacion { get; set; }
+        [BindProperty]
+        public string EstadoFormacion { get; set; }
+        [BindProperty]
+        public string CiudadFormacion { get; set; }
+        [BindProperty]
+        public string MesInicioFormacion { get; set; }
+        [BindProperty]
+        public string AnioInicioFormacion { get; set; }
+        [BindProperty]
+        public string MesFinalFormacion { get; set; }
+        [BindProperty]
+        public string AnioFinalFormacion { get; set; }
+        public IList<Certificaciones> ListaCertificaciones { get; set; }
+        public IList<Idiomas> ListaIdiomas { get; set; }
+
 
 
 
@@ -119,7 +175,7 @@ namespace Atos.Pages
 
             reader3.Close();
             
-            cmd.CommandText = "SELECT * FROM sobremi where idAplicante = " + idAplicante;
+            cmd.CommandText = "SELECT u.idAplicante, u.AcercaDeMi, u.Habilidad1, u.Habilidad2, u.Habilidad3, u.Habilidad4, u.Habilidad5, r.PorcHabilidad1, r.PorcHabilidad2, r.PorcHabilidad3, r.PorcHabilidad4, r.PorcHabilidad5 FROM sobremi u JOIN  habilidades r ON u.idAplicante = r.idAplicante where u.idAplicante = " + idAplicante;
             var reader4 = cmd.ExecuteReader();
             reader4.Read();
 
@@ -130,16 +186,91 @@ namespace Atos.Pages
             Habilidad4 = reader4["Habilidad4"].ToString();
             Habilidad5 = reader4["Habilidad5"].ToString();
             
+            PorcHabilidad1 = reader4.GetInt32(reader4.GetOrdinal("PorcHabilidad1"));
+            PorcHabilidad2 = reader4.GetInt32(reader4.GetOrdinal("PorcHabilidad2"));
+            PorcHabilidad3 = reader4.GetInt32(reader4.GetOrdinal("PorcHabilidad3"));
+            PorcHabilidad4 = reader4.GetInt32(reader4.GetOrdinal("PorcHabilidad4"));
+            PorcHabilidad5 = reader4.GetInt32(reader4.GetOrdinal("PorcHabilidad5"));
 
-            reader3.Close();
+            PorcHabilidad1_ = reader4["PorcHabilidad1"].ToString() + "%";
+            PorcHabilidad2_ = reader4["PorcHabilidad2"].ToString() + "%";
+            PorcHabilidad3_ = reader4["PorcHabilidad3"].ToString() + "%";
+            PorcHabilidad4_ = reader4["PorcHabilidad4"].ToString() + "%";
+            PorcHabilidad5_ = reader4["PorcHabilidad5"].ToString() + "%";
+
+
+            reader4.Close();
 
 
 
-            conexion.Dispose();
+            cmd.CommandText = "SELECT u.idAplicante, u.PuestoExperiencia, u.EmpresaExperiencia, u.AnioExperienciaInicio, u.AnioExperienciaFin, u.Actividades, r.MesEscrito as 'MesExperienciaInicio', z.MesEscrito as 'MesExperienciaFin' FROM experienciaprofesional u JOIN  meses r ON u.MesExperienciaInicio = r.idMes JOIN meses z ON u.MesExperienciaFin = z.idMes where u.idAplicante = " + idAplicante;
+            var reader5 = cmd.ExecuteReader();
+            reader5.Read();
+
+            PuestoExperiencia = reader5["PuestoExperiencia"].ToString();
+            EmpresaExperiencia = reader5["EmpresaExperiencia"].ToString();
+            AnioExperienciaInicio = reader5["AnioExperienciaInicio"].ToString();
+            AnioExperienciaFin = reader5["AnioExperienciaFin"].ToString();
+            Actividades = reader5["Actividades"].ToString();
+            MesExperienciaInicio = reader5["MesExperienciaInicio"].ToString();
+            MesExperienciaFin = reader5["MesExperienciaFin"].ToString();
+
+            reader5.Close();
+
+            cmd.CommandText = "SELECT u.idAplicante, u.Institucion, u.Carrera, u.Pais, u.Estado, u.Ciudad, u.AnioInicioFormacion, u.AnioFinalFormacion, r.MesEscrito as 'MesInicioFormacion', z.MesEscrito as 'MesFinalFormacion' FROM formacionacademica u JOIN  meses r ON u.MesInicioFormacion = r.idMes JOIN meses z ON u.MesFinalFormacion = z.idMes where u.idAplicante = " + idAplicante;
+            var reader6 = cmd.ExecuteReader();
+            reader6.Read();
+
+            Institucion = reader6["Institucion"].ToString();
+            Carrera = reader6["Carrera"].ToString();
+            PaisFormacion = reader6["Pais"].ToString();
+            EstadoFormacion = reader6["Estado"].ToString();
+            CiudadFormacion = reader6["Ciudad"].ToString();
+            MesInicioFormacion = reader6["MesInicioFormacion"].ToString();
+            MesFinalFormacion = reader6["MesFinalFormacion"].ToString();
+            AnioFinalFormacion = reader6["AnioFinalFormacion"].ToString();
+            AnioInicioFormacion = reader6["AnioInicioFormacion"].ToString();
+
+            reader6.Close();
+
+            cmd.CommandText = "SELECT Idioma, Nivel FROM idiomas where idAplicante = " + idAplicante;
+
+
+            Idiomas idioma1 = new Idiomas();
+            ListaIdiomas = new List<Idiomas>();
+
+            using (var reader7 = cmd.ExecuteReader())
+            {
+                while (reader7.Read())
+                {
+                    idioma1 = new Idiomas();
+                    idioma1.Idioma = reader7["Idioma"].ToString();
+                    idioma1.Nivel = reader7["Nivel"].ToString();
+                    ListaIdiomas.Add(idioma1);
+                }
+            }
 
             
 
 
+            cmd.CommandText = "SELECT Certificacion, Anio FROM certificacion where idAplicante = " + idAplicante;
+            
+
+            Certificaciones cert1 = new Certificaciones();
+            ListaCertificaciones = new List<Certificaciones>();
+
+            using (var reader8 = cmd.ExecuteReader())
+            {
+                while (reader8.Read())
+                {
+                    cert1 = new Certificaciones();
+                    cert1.Certi = reader8["Certificacion"].ToString();
+                    cert1.Anio = reader8.GetInt32(reader8.GetOrdinal("anio"));
+                    ListaCertificaciones.Add(cert1);
+                }
+            }
+
+            conexion.Dispose();
 
         }
 
