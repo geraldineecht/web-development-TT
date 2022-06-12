49c3056e3a68344fd6cfcb3068bfc99e0cb00b83
Carmina López <a00830265@itesm.mx>
2022-06-12 17:20:30 -0500

    Post Entrevista y Get idAplicante
    Se crearon dos clases controllers, en una se recibe un get idAplicante y lo regresa en JSON y la otra actualiza la base de datos.

--- AplicacionWeb/ASP.net/Login/Login/Controllers/ApplicantScoreController.cs
+++ AplicacionWeb/ASP.net/Login/Login/Controllers/ApplicantScoreController.cs
@@ -6,51 +6,31 @@ using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using Microsoft.AspNetCore.Http;
 
+
+
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Atos.Controllers
 {
     [ApiController]
-    [Route("api/entrevista/resultados")]
-
-
+    [Route("api/entrevista")]
     public class ApplicantScoreController : ControllerBase
     {
-        public int idUsuario { get; set; }
-        public string scoreAplicante { get; set; }
-
-
-        //// GET: api/values
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
-
-        //// GET api/values/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        
         [HttpPost]
-        //public void Post([FromBody] string value)
-        public void Post(int scoreEntrevista)
+        //public void Post([FromBody] string value) [FromQuery] int scoreEntrevista
+        public int Post([FromQuery] int scoreEntrevista, int idUsuario)
         {
-            idUsuario = (int)HttpContext.Session.GetInt32("idAplicante");
-
             string connectionString = "Server=127.0.0.1;Port=3306;Database=Atos;Uid=root;password=Gato1415*;";
             MySqlConnection conexion = new MySqlConnection(connectionString);
             conexion.Open();
             MySqlCommand cmd = new MySqlCommand();
             cmd.Connection = conexion;
-            cmd.CommandText = "INSERT INTO entrevista(idAplicante, fecha, puntaje) VALUES (" + idUsuario + ", " + scoreEntrevista + ", CURDATE());";
+            cmd.CommandText = "INSERT INTO Entrevista(idAplicante, fecha, puntaje) VALUES (" + idUsuario + ", CURDATE()," + scoreEntrevista + ");";
 
             cmd.ExecuteNonQuery();
             conexion.Close();
 
+            return scoreEntrevista + idUsuario;
 
         }
 
